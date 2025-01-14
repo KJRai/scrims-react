a95b9a047e4dd9a0206d54912ed66b807474ec39
KJRai <alexrayee289@gmail.com>
2025-01-14 10:25:42 +0545

    added

--- 01. Static pages/04. First React Challenge/index.jsx
+++ 01. Static pages/04. First React Challenge/index.jsx
@@ -3,3 +3,10 @@
  * This time, try rendering an unordered list with 2-3 list items inside
  * with why you're excited to be learning React.
  */
+  import { React } from "react"
+   const root = createRoot(document.getElementById("root"))
+   root.render(<ul>
+    <li>react is fun</li>
+    <li>react do be the shit</li>
+    <li>react goes brrrr</li>
+   </ul>)
